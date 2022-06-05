a06cfb366691b1c6825c330f345a8c7903f00032
KevinSlim67 <kevinslim167@gmail.com>
2022-06-05 22:33:22 +0300

    MyFile class addition

--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Document document = new Document("Breaking Bad", Arrays.asList("Kevin", "Charbel"));
-        System.out.println(document.toString());
-
-
-        Document document2 = new Document("The Walking Dead", Arrays.asList("Aline", "Elie"));
-        System.out.println(document2.toString());
+        MyFile file = new MyFile("test.txt");
+        file.deleteAll();
 
     }
 }
--- MyFile.java
+++ MyFile.java
@@ -1,8 +1,51 @@
 import java.io.File;
+import java.io.FileWriter;
 
-public class MyFile implements Searchable{
+public class MyFile implements Searchable {
 
     private File file;
+
+    public MyFile(String fileName) {
+        try {
+            file = new File(fileName);
+            if (!file.exists()) file.createNewFile();
+        } catch (Exception e) {
+            System.out.println("Sorry, we were not able to create the file!");
+        }
+    }
+
+    public boolean add(String word) {
+        FileWriter fw;
+
+        try {
+            fw = new FileWriter(file, true);
+            fw.write(word + "\r\n");
+            fw.close();
+            System.out.println("String successfully added to file");
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Sorry, we could not append to file");
+        }
+
+        return false;
+    }
+
+    public boolean deleteAll() {
+        FileWriter fw;
+
+        try {
+            fw = new FileWriter(file, false);
+            fw.close();
+            System.out.println("Successfully deleted all data from file!");
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Sorry, could not delete data");
+        }
+
+        return false;
+    }
 
     @Override
     public boolean hasKeyword(String word) {
